fe61e81d54b964edd3b6c2ec05acb01ff150eed7
Evgeny Borisenko <borisenkoevgeny1991@gmail.com>
2019-12-02 19:04:39 +0200

    add active class  (eight__section)

--- index.js
+++ index.js
@@ -1,38 +1,9 @@
-// --------------animation scroll-----------------
-$(document).ready(function() {
-  // плавное перемещение страницы к нужному блоку
-  $('nav li a').click(function() {
-    elementClick = $(this).attr('href');
-    destination = $(elementClick).offset().top;
-    $('body,html').animate({ scrollTop: destination }, 800);
-  });
-});
-
 // --------------open / close   questions---------------
 $('.reviews-item').click(function(event) {
   $(this).toggleClass('reviews-item--active');
 });
 
-// --------------------burger---------------------
-
-const burgerButton = document.querySelector('.burger__button');
-const burgerMenu = document.querySelector('.burger__menu');
-const shadowBox = document.querySelector('.shadow__box');
-
-function openBurgerWindow(e) {
-  if (event.target === shadowBox) {
-    return;
-  }
-  burgerMenu.classList.add('burger__menu__open');
-  shadowBox.classList.add('shadow__box__active');
-}
-burgerButton.addEventListener('click', openBurgerWindow);
-
-function closeBurgerWindow(e) {
-  if (event.target !== event.currentTarget) {
-    return;
-  }
-  burgerMenu.classList.remove('burger__menu__open');
-  shadowBox.classList.remove('shadow__box__active');
-}
-shadowBox.addEventListener('click', closeBurgerWindow);
+// -------------open / close   teach block-------------
+$('.eight__section__nav__item').click(function(event) {
+  $(this).toggleClass('eight__section__nav__item__active');
+});
